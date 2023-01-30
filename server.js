8f8664d78d5f815e94968a52f8183a69095682f0
itsisrael <bo66cobra99@gmail.com>
2023-01-30 11:15:11 +0100

    Update server :octocat:

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world Express js")
+  res.send("<h1> Server Express with Nodejs  and HTML</h1>")
 })
 
 app.listen(3000);
